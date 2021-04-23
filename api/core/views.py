e22f73333ea35a7a21bbe8f40919d5489a43f1ca
MareksNo <ulmanismareks@gmail.com>
2021-04-23 22:18:41 +0100

    fixed my dumbass

--- api/core/views.py
+++ api/core/views.py
@@ -1,18 +1,19 @@
-import django_filters.rest_framework
 from rest_framework import viewsets
-from core.serializers import QuestionSerializer, OptionSerializer, MemberSerializer
+
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
+import django_filters.rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
 
+from core.serializers import QuestionSerializer, OptionSerializer, MemberSerializer
 from core.models import Question, Option, Member
 
 
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    authentication_classes = [TokenAuthentication] # Autentifikācija ar token
+    permission_classes = [IsAuthenticatedOrReadOnly] # Iespējams GET bes auth, bet POST nepieciešams auth
 
     serializer_class = QuestionSerializer
 
@@ -22,10 +23,10 @@ class OptionViewSet(viewsets.ModelViewSet):
     serializer_class = OptionSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['question', 'correct']
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    authentication_classes = [TokenAuthentication] # Autentifikācija ar token
+    permission_classes = [IsAuthenticatedOrReadOnly] # Iespējams GET bes auth, bet POST nepieciešams auth
 
 
 class MemberViewSet(viewsets.ModelViewSet):
     queryset = Member.objects.all().order_by('-score')
-    serializer_class = MemberSerializer
+    serializer_class = MemberSerializer 
